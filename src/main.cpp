607a51eb4ebc912d86d62aa4d3b40f323a61bf41
Sir-Kuhnhero <85761499+Sir-Kuhnhero@users.noreply.github.com>
2022-07-16 19:43:00 +0200

    basic radio communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,46 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
+
+bool reciveData(){
+  bool recived = false;
+  long time = millis();
+
+  while(!recived)
+  {
+    if (radio.available()) {
+      char text[32] = "";
+      radio.read(&text, sizeof(text));
+      Serial.println(text);
+
+      recived = true;
+    }
+
+    if (millis() - time > 1000)
+      return false;
+  }
+
+  return true;
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!reciveData())
+    Serial.println("no data connection!!!");
+  
+  delay(10);
 }
