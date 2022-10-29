81f99b85d322350681bf2418ecad3520340a2779
Sir-Kuhnhero <85761499+Sir-Kuhnhero@users.noreply.github.com>
2022-10-29 13:48:15 +0200

    IMU

--- receiver/reveiver_code/include/sensor.h
+++ receiver/reveiver_code/include/sensor.h
@@ -3,6 +3,7 @@
 // initialize data shared between .cpp files
 #define VOLTAGE
 #define IMU
+#define BMP280
 
 #ifdef VOLTAGE
     struct Voltage {
@@ -17,3 +18,59 @@
     void Voltage_init();
 #endif
 
+#ifdef IMU
+    #include "helper_3dmath.h"
+    // uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+    // quaternion components in a [w, x, y, z] format (not best for parsing
+    // on a remote host such as Processing or something though)
+    //#define OUTPUT_READABLE_QUATERNION
+
+    // uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+    // (in degrees) calculated from the quaternions coming from the FIFO.
+    // Note that Euler angles suffer from gimbal lock (for more info, see
+    // http://en.wikipedia.org/wiki/Gimbal_lock)
+    //#define OUTPUT_READABLE_EULER
+
+    // uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+    // pitch/roll angles (in degrees) calculated from the quaternions coming
+    // from the FIFO. Note this also requires gravity vector calculations.
+    // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+    // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+    #define OUTPUT_READABLE_YAWPITCHROLL
+
+    // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+    // components with gravity removed. This acceleration reference frame is
+    // not compensated for orientation, so +X is always +X according to the
+    // sensor, just without the effects of gravity. If you want acceleration
+    // compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+    //#define OUTPUT_READABLE_REALACCEL
+
+    // uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+    // components with gravity removed and adjusted for the world frame of
+    // reference (yaw is relative to initial orientation, since no magnetometer
+    // is present in this case). Could be quite handy in some cases.
+    //#define OUTPUT_READABLE_WORLDACCEL
+
+
+    extern Quaternion q;           // [w, x, y, z]         quaternion container
+    extern VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+    extern VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+    extern VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+    extern VectorFloat gravity;    // [x, y, z]            gravity vector
+    extern float euler[3];         // [psi, theta, phi]    Euler angle container
+    extern float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+    bool IMU_init();
+    bool IMU_read();
+#endif
+
+#ifdef BMP280
+    #include <Adafruit_BMP280.h>
+    
+    Adafruit_BMP280 bmp; // use I2C interface
+    Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+    Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+
+    sensors_event_t temp_event, pressure_event;
+#endif
--- receiver/reveiver_code/src/sensor.cpp
+++ receiver/reveiver_code/src/sensor.cpp
@@ -33,3 +33,174 @@ void Voltage_read()
     }
 }
 #endif
+
+#ifdef IMU
+// ================================================================
+// ===                            IMU                           ===
+// ================================================================
+#include <MPU6050_6Axis_MotionApps20.h>
+#include "MPU6050.h"
+
+
+
+MPU6050 mpu;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+bool IMU_init() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+		#if defined (PARTICLE)
+		  	Wire.setSpeed(CLOCK_SPEED_400KHZ);
+		  	Wire.begin();
+		#else
+		  	Wire.begin();
+		  	TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+		#endif
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+	    #if defined (PARTICLE)
+            attachInterrupt(D2, dmpDataReady, RISING);
+	    #else
+            attachInterrupt(2, dmpDataReady, RISING);
+	    #endif
+            mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+        return false;
+    }
+    return true;
+}
+
+
+bool IMU_read() {
+    if (!mpuInterrupt && fifoCount < packetSize) {
+        return false;
+    }
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.print(F("FIFO overflow!   "));
+        return false;
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } 
+    else if ((mpuIntStatus & 0x02) > 0) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // read quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+        #endif
+
+        #ifdef OUTPUT_READABLE_EULER
+            // read Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+        #endif
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // read Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        #endif
+
+        #ifdef OUTPUT_READABLE_REALACCEL
+            // read real acceleration, adjusted to remove gravity
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // read initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+        #endif
+    }
+    return true;
+}
+#endif
